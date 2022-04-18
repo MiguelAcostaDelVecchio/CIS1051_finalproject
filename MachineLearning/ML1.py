aad5f5182972b87be7f178bf05ad7fb65cac1029
Miguel <macostadelvecchio@gmail.com>
2022-04-18 10:38:53 -0400

    added some results obtained from scripts

---
+++ MachineLearning/ML1.py
@@ -0,0 +1,13 @@
+#Import libraries
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#%matolotlib inline
+
+dataset_train = pd.read_csv("Google_Stock_Price_Train.csv")
+dataset_train.head()
+
+training_set = dataset_train.iloc[:,1:2].values
+print (training_set)
+print (training_set.shape)
